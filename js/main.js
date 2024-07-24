0afc0e2752cc5800949408bdba902c76c9e54192
mrAnderson90 <115795889+mrAnderson90@users.noreply.github.com>
2024-07-24 23:17:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Находим элементы
+
+const btn = document.querySelector('#btn-mobile-nav');
+const mobileBtnImg = document.querySelector('#mobile-nav-img');
+const mobileNav = document.querySelector('#mobile-nav');
+const header = document.querySelector('#header');
+
+// Добавлякм прослушку по клику
+
+btn.addEventListener('click', function() {
+	btn.classList.toggle('open');
+	mobileNav.classList.toggle('open');
+	header.classList.toggle('header-white-bg');
+	document.body.classList.toggle('no-scroll');
+
+	if (btn.classList.contains('open')) {
+		mobileBtnImg.src = './img/icons/close-mobile-nav.svg';
+	} else {
+		mobileBtnImg.src = './img/icons/open-mobile-nav.svg';
+	}
+});
